3e7e3eed17a1811d5b1378644e47856e4f2e0897
niels <nielsvdmarel@gmail.com>
2020-04-18 21:58:02 +0200

    Player movement, animations and working moveto function.

---
+++ Assets/Scripts/Player/Animation/PlayerAnimationController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    Animator animator;
+    AnimatorStateInfo stateInfo;
+    PlayerBehaviour playerBehaviour;
+    void Start() {
+        animator = this.GetComponent<Animator>();
+        playerBehaviour = this.GetComponent<PlayerBehaviour>();
+    }
+
+    void Update() {
+        checkAnimationState();
+    }
+
+    void OnAnimatorMove() {
+        stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        if (!stateInfo.IsTag("Walking") || !stateInfo.IsTag("Idle")) {
+            animator.ApplyBuiltinRootMotion();
+        }
+    }
+
+    void checkAnimationState() {
+
+        if (playerBehaviour.facingRight) {
+            animator.SetFloat("RightLeft", 1);
+        }
+        else if (!playerBehaviour.facingRight) {
+            animator.SetFloat("RightLeft", -1);
+        }
+
+        if(playerBehaviour.currentPlayerState == PlayerBehaviour.playerState.idle) {
+            animator.SetBool("Idle", true);
+        }
+        else {
+            animator.SetBool("Idle", false);
+        }
+        if (playerBehaviour.currentPlayerState == PlayerBehaviour.playerState.rotating) {
+            animator.SetBool("Turning", true);
+        }
+        else {
+            animator.SetBool("Turning", false);
+        }
+        if (playerBehaviour.currentPlayerState == PlayerBehaviour.playerState.moving) {
+            animator.SetBool("Walking", true);
+        }
+        else {
+            animator.SetBool("Walking", false);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Movement/PlayerBehaviour.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerBehaviour : MonoBehaviour {
+    public enum playerState{
+        idle,
+        rotating,
+        moving,
+        climbing,
+        death
+    }
+
+    [SerializeField]
+    public playerState currentPlayerState;
+    public bool facingRight;
+
+    private Vector3 targetPosition;
+    private Quaternion targetRotation;
+
+    [SerializeField]
+    float playerMovementSpeed;
+
+    void Start() {
+        currentPlayerState = playerState.idle;
+        targetPosition = new Vector3(5, 0, 5);
+       // StartCoroutine(PlayerStructure());
+    }
+
+    void Update() {
+        if(currentPlayerState == playerState.moving) {
+            //Debug.Log("Moving");
+            PlayerWalk();
+            float distance = Vector3.Distance(transform.position, targetPosition);
+            if (distance < 0.1f) {
+                transform.position = targetPosition;
+                currentPlayerState = playerState.idle;
+            }
+        }
+
+        if (currentPlayerState == playerState.rotating) {
+            //Debug.Log("rotating");
+            if (1 - Mathf.Abs(Quaternion.Dot(transform.rotation, targetRotation)) < 0.001f) {
+                currentPlayerState = playerState.moving;
+            }
+        }
+
+        if (currentPlayerState == playerState.idle) {
+            //Debug.Log("Idle");
+        }
+    }
+
+    private void PlayerWalk() {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * playerMovementSpeed);
+    }
+
+    private void setRotationTarget() {
+        Vector3 lookPos = targetPosition - transform.position;
+        lookPos.y = 0;
+        targetRotation = Quaternion.LookRotation(lookPos);
+    }
+
+    public void setTurnSide() {
+        Vector3 cross = Vector3.Cross(transform.rotation * Vector3.forward, targetRotation * Vector3.forward);
+        facingRight = cross.y < 0;
+        Debug.Log(facingRight);
+    }
+
+    //For actual useage in game
+    public void SetTarget(UnityEngine.Vector3 position) {
+        targetPosition = position;
+        setRotationTarget();
+        setTurnSide();
+    }
+
+    public void moveTo() {
+        currentPlayerState = playerState.rotating;
+    }
+}
