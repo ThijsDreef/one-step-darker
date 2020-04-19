61797ef9a03662c4dcbb4b7aab159da5c9836f17
niels <nielsvdmarel@gmail.com>
2020-04-20 01:39:45 +0200

    lamp v1

---
+++ Assets/Scripts/Lamp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour {
+    float value = 180;
+
+    [SerializeField]
+    float speed;
+
+    float delta = 10.0f;
+    void Start() {
+        
+    }
+
+    void Update() {
+        transform.rotation = Quaternion.Euler(0, 0, 180);
+    }
+}
